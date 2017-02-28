3ccb7adcaf4cdea3a8fb755853b67eb416fad7ca
Palant Pierre-Louis <ppalant@SA203-07A.ensi-bourges.fr>
2017-02-28 14:25:28 +0100

    modif mineures serveur

--- serveur/projserver.py
+++ serveur/projserver.py
@@ -2,34 +2,20 @@
 
 
 
-import socket
-
-import os
-
-import sys
-
-
+import socket,sys,os
 
 TCP_IP  = '127.0.0.1'
-
 TCP_PORT = 6262
-
-
-
-BUFFER_SIZE = 20
-
-
+BUFFER_SIZE = 1024
 
 s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 s.bind((TCP_IP,TCP_PORT))
 
-s.listen(1)
-
-
+s.listen(5)
 
 conn, addr = s.accept()
-
+print('Connection entrante :', addr)
 
 
 data = conn.recv(BUFFER_SIZE)
