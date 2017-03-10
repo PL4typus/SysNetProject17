1fddb7cc55c60e3d692a7dccaa596fd849e9c362
Armellys <adele.journee@hotmail.fr>
2017-03-10 09:59:51 +0100

    réajustement multiclient et droit

--- serveur/module_thread.py
+++ serveur/module_thread.py
@@ -4,30 +4,29 @@
 import sys,socket,os,threading
 
 class ClientThread(threading.Thread):
-	
-	def __init__(self, ip, port, clientsocket):
-		self.BUFFER_SIZE = 2048
-		threading.Thread.__init__(self)
-		self.ip = ip
-		self.port = port
-		self.conn = clientsocket
-		print('## Nouveau thread pour ', self.ip,': ', self.port)
-
-	def run(self):
-		data = self.conn.recv(self.BUFFER_SIZE)
-		data=data.decode()
-		if data == "m" :
-  			os.popen("chmod +w $PWD")
-		else :
-  			os.popen("chmod -w $PWD")
-
-		while 1 :
+  	
+  	def __init__(self, ip, port, clientsocket):
+  		self.BUFFER_SIZE = 2048
+  		threading.Thread.__init__(self)
+  		self.ip = ip
+  		self.port = port
+  		self.conn = clientsocket
+  		print('## Nouveau thread pour ', self.ip,': ', self.port)
+  
+  	def run(self):
+  		data = self.conn.recv(self.BUFFER_SIZE)
+  		droit=data.decode()
+  		while 1 :
   			data = self.conn.recv(self.BUFFER_SIZE)
   			data= data.decode()
-  			print( data)
+  			if droit == "m" :
+  				os.popen("chmod +w $PWD")
+  			else :
+  				os.popen("chmod -w $PWD")
+  			print (data)
   			if data == "1":
     				break 
-
+  
   			rep = os.popen(data+" 2>&1")
   			reponse="reponse: \n"+rep.read()
   			self.conn.send(reponse.encode())
