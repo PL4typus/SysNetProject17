b738a12cc8a6d1cb98df15a28ff73a98ba1ffd78
chaR94 <charlotte.rambourg@insa-cvl.fr>
2017-03-10 11:33:48 +0100

    login presqueOKCLIENT/Serveur

--- Authentification/login.py
+++ Authentification/login.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3.4
 #Client authentification
 #coding: utf8
 import os
@@ -6,6 +6,18 @@ from getpass import getpass
 import hashlib
 
 
+import socket
+TCP_IP = '127.0.0.1'
+TCP_PORT=6262
+BUFFER_SIZE=100
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+s, addr = s.accept()
+
+print ("Connection adresse:",addr)
 
 mdpMed="azerty"
 Med="medecin"
@@ -23,16 +35,6 @@ def lecture_fichier(fichier) :
 		l[i] = l[i].split(':')
 	return l
 
-def lecture_fichier(fichier) :
-	f = open(fichier,'r')
-	fo = f.read(1024)
-	fo=fo.rstrip()
-	l = fo.split(';') 
-	for i in range(len(l)) :
-		l[i] = l[i].split(':')
-	return l
-
-
 
 def LOGIN():
 	tout = True
@@ -45,48 +47,57 @@ def LOGIN():
 	while tout :
 		user = ''
 		while service:
-			print ("service")
-			service=conn.recv(10)
+			
+			service=s.recv(10)
+			service=service.decode()
 			if service == "Medecin":
 				l=lecture_fichier("passwordMed.txt")
 				service = False
+				s.send(b"1")
 			elif service == "Infirmier":
 				l=lecture_fichier("passwordInf.txt")
 				service = False
+				s.send(b"2")	
 			elif service == "Interne":
 				l=lecture_fichier("passwordInt.txt")
 				service = False
+				s.send(b"3")
 			else:
-				print("Ce service n'existe pas")
+				s.send(b"0")
 			
 		while session and user != 'retour':
 
-			print("session")
-			user= conn.recv(20)
+			
+			user= s.recv(20)
+			user=user.decode()
 
 			if user == 'retour' :
 				service = True
+				s.send(b"return")
 				break
 			else :
 		
 				for i in range(len(l)):
 					for j in range(len(l[i])):
 						if user == l[i][0]:
-							print ("Utilisateur reconnu")
+							s.send(b"1")
 							session = False
 							tout = False
 				if session == True:	
-					print("Utilisateur inconnu")		
-	
+					s.send(b"0")	
+			s.recv(30)
 		while verrouille and tout == False :
 			time-=1
-			print("Reste",time,"essai")
+			Timeline="Reste "+str(time)+" essai"
+			Timeline=Timeline.encode()
+			s.send(Timeline)
 			
-			if time ==0:
-				print("Plus d'essai disponible")
+			if time == 0:
+				s.send(b"0")
 				break
 			else:
-				saisie = conn.recv(30)
+				saisie = s.recv(30)
+				saisie=saisie.decode()
 				hash_mdp = hashlib.sha256(saisie.encode()).hexdigest()
 
 			
@@ -95,10 +106,12 @@ def LOGIN():
 					if user == l[i][0]:
 						if hash_mdp == l[i][1]:
 							verrouille=False
-							print("Session ouverte")
+							s.send(b"1")
 						else:
-							print("Mot de passe incorrect")
+							s.send(b"0")
+	s.close()
 
 LOGIN()
+print("Fin serveur")
 
 
--- Authentification/loginClient.py
+++ Authentification/loginClient.py
@@ -1,24 +1,107 @@
-#!/usr/bin/python
+#!/usr/bin/python3.4
 #client authentification
 import os
 from getpass import getpass
 #coding: utf8
 
+import socket
+
+localhost = '127.0.0.1'
+port = 6262
+BUFFER_SIZE=100
+
+# Set up a TCP/IP socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+# Connect as client to a selected server
+# on a specified port
+s.connect((localhost,port))
 
 saisie=b""
 
 print("=====================================================================================")
 print("<<<<<<<<<<<<<<<<<<<<<<<<<Bienvenu sur le serveur de l'hopital>>>>>>>>>>>>>>>>>>>>>>>>")
-print("Voulez vous:\n\t¤Vous inscrire (signup)?\n\t¤Vous connecter(login)?\n\t¤Quitter(exit)?)
+print("Voulez vous:\n\t¤Vous inscrire (signup)?\n\t¤Vous connecter(login)?\n\t¤Quitter(exit)?")
 
 while saisie!= "exit":
-	
+	service=True
+	session=True
+	tout=True
+	verrouille=True
+
 	saisie=input(">> ")
 	
-	if saisie == "signup":
-		
+	#if saisie == "signup":
 	if saisie == "login":
+		
+		while tout:
+			user=''
+			while service:
+				saisie=input("Quel service? (Medecin, Infirmiere, Interne)")
+				saisie=saisie.encode()
+				s.send(saisie)
+		
+				data=s.recv(10)
+				data=data.decode()
+		
+				if data == "1":
+					print("Vous etes un Médecin")
+					service=False
+				elif data == "2":
+					print("Vous etes un Infirmier")
+					service=False
+				elif data == "3":
+					print("Vous etes un Interne")
+					service=False
+				else:
+	
+					print("Service inconnu")
 
+			while session and user != 'retour':
 
+				user=input("Utilisateur: ")
+				user=user.encode()
+				s.send(user)
+		
+				data=s.recv(30)
+				data=data.decode()	
+				print(data)
+				s.send(b"OK")
+				if data == "11":
+					session=False
+					tout = False
+					print("Utilisateur reconnu")
+				elif data == "return":
+					service=True
+					break
+				else:
+					print ("Utilisateur inconnu")
+
+			while verrouille and tout == False :
+			
+				Timeline=s.recv(30)
+				Timeline=Timeline.decode()
+				print(Timeline)
+				
+				if  Timeline == "Reste 0 essai":
+					print("Plus d'essai disponible")
+					break
+				else:
+					saisie=input("Mot de passe: ")
+					saisie=saisie.encode()
+					s.send(saisie)
+				
+				verif=s.recv(10)
+				verif=verif.decode()
+				print (verif)
+				if verif == "11":
+					print("Session ouverte")
+					verrouille = False
+				else:
+					print("Mot de passe incorrect")
+			
+	
 print("<<<<<<<<<<<<<<<<<<<<<<<<<Déconnexion du serveur de l'hopital>>>>>>>>>>>>>>>>>>>>>>>>")
 print("=====================================================================================")
+
+s.close()
