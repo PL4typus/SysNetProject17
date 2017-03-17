99a2b8444068769fcd50ec07738986ba70831eb5
PL <pierre-louis.palant@insa-cvl.fr>
2017-03-17 10:52:37 +0100

    Traitement de texte + command checker opérationnel sauf mkdir

--- TraitementTxtFork/serveurTxt.py
+++ TraitementTxtFork/serveurTxt.py
@@ -12,6 +12,57 @@ s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
 s.bind((TCP_IP,TCP_PORT))
 
+def command_checker(command, status,conn, ip, port):
+
+	command = command.split(' ')
+	buf=os.popen("ls user")
+	buf= buf.read()
+
+	if command[0] == "ls":#modifier options
+		rep=os.popen("ls user -go")
+		reponse =rep.read()
+		conn.send(reponse.encode())
+	elif command[0] == "cat":
+		if command[1] not in buf:
+			reponse = "Vous n'avez pas l'autorisation de faire ça, ou le fichier demandé n'existe pas!\n"
+		else:
+			cccp="cat user/"+command[1]
+			rep=os.popen(cccp)
+			reponse=rep.read()
+		conn.send(reponse.encode())
+	elif command[0] == "rm":
+		if status=="m": #a modifier avec login()
+			if command[1] not in buf:
+				reponse = "Vous n'avez pas l'autorisation de faire ça, ou le fichier demandé n'existe pas!\n"
+			else:
+				reponse = "Etes-vous sûr(e) de vouloir supprimer le fichier "+command[1] +"?   O/N ? \t Il ne sera pas recuperable."
+				ok = False
+				while ok == False:				
+					conn.send(reponse.encode())
+					data=conn.recv(2048).decode()
+					if data in {"yes","oui","o","ouais","y","O","YES","Y","OUI","OUAIS"}:
+						cccp="rm -vf user/"+command[1]
+						print(cccp)
+						rep=os.popen(cccp)
+						reponse=rep.read()
+						ok = True
+					elif data in {"no","non", "n","NON","N","NO","NEIN"}:
+						reponse = "Ok, fichier non supprimé!"
+						ok = True
+		else:#modifier avec login()
+			reponse="Vous n'avez pas l'autorisation de supprimer des fichiers!"
+		conn.send(reponse.encode())
+
+	elif command[0] == "mkdir":
+		#wip
+		print("lol")
+	elif command[0] == "edit": #nom du fichier
+		#wip
+		EDIT(conn, command[1])
+	elif command[0] == "creer": #nom du fichier
+		#wip
+		CREER(conn,command[1])
+
 def EDIT (conn, nomF):
 	r = os.popen("cd user/;cat "+nomF+" 2>&1")
 	err = os.popen("cd user/;cat "+nomF+" 1>&2;echo $?");#pour voir si le fichier existe
@@ -83,22 +134,21 @@ def barman(conn,ip,port):
 			else :
 				os.popen("cd user/;chmod -w $PWD")
 			print (data)
-			l = data.split(" ")#split pour si il a plusieur argument
-
+			command_checker(data,droit,conn,ip, port)
 #----------------------------------------------
-			if l[0]== "edit" and droit == "m": #pour editer un texte seul les medecins peuvent
-				EDIT(conn,l[1])
+#			if l[0]== "edit" and droit == "m": #pour editer un texte seul les medecins peuvent
+#				EDIT(conn,l[1])
 #---------------------------------------------------------
-
-			elif l[0] == "creer" and droit == "m":
-				CREER(conn,l[1])
+#
+#			elif l[0] == "creer" and droit == "m":
+#				CREER(conn,l[1])
 #---------------------------------------------------------
-			elif l[0] == "1":
-				break 
-			else :
-				rep = os.popen("cd user/;"+data+" 2>&1")
-				reponse="reponse: \n"+rep.read()
-				conn.send(reponse.encode())
+#			elif l[0] == "1":
+#				break 
+#			else :
+#				rep = os.popen("cd user/;"+data+" 2>&1")
+#				reponse="reponse: \n"+rep.read()
+#				conn.send(reponse.encode())
 while True:
 	s.listen(5)
 	print('En écoute sur ', TCP_PORT,'...')
