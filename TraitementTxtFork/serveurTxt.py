c16ed5997f1ac46c8a6df1696c14f827ab3a7eaa
Armellys <adele.journee@hotmail.fr>
2017-03-23 10:06:14 +0100

    petit chgmt insignifiant

--- TraitementTxtFork/serveurTxt.py
+++ TraitementTxtFork/serveurTxt.py
@@ -164,11 +164,11 @@ def command_checker(command, status,conn, ip, port):
 		#wip
 		print("lol")
 	elif command[0] == "edit": #nom du fichier
-		#wip
+		
 		EDIT(conn, command[1])
 
 	elif command[0] == "creer": #nom du fichier
-		#wip
+	
 		CREER(conn,command[1])
 
 def EDIT (conn, nomF):
